66533e83ccacdc8fc72726c4ebb54c3d07adae5d
Khan <khan@Khans-Mac-mini.local>
2016-08-02 14:39:57 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 apt-get install sshpass
-sshpass -p 'pass@word1' ssh namcha@54.169.173.27 'echo test > /home/test.txt'
+sshpass -p 'pass@word1' ssh namcha@54.169.173.27 'echo test > /home/namcha/test.txt'
