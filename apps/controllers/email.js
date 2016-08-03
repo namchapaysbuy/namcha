3b1b75b8c25f59bac3259b35e20cf9baea4f9f03
user5 <user5@gmail.com>
2016-08-03 11:44:30 +0700

    add email validate send only correct format

--- apps/controllers/email.js
+++ apps/controllers/email.js
@@ -2,14 +2,36 @@
 
 require('rootpath')()
 
+let helper = require('apps/helpers')
 let mongo = require('config/mongo')
 let User  = require('apps/models/user')
 
 exports.getMainpage = (req, res) => {
   res.render('index.ejs', { title: 'oo' })
-
 }
 
 exports.postEmail = (req, res) => {
-  res.send('send e-mail successful')
+  const emailTo  = req.body.to
+  const emailTopic = req.body.topic
+  const emailBody = req.body.body
+
+  let emails = []
+
+  if (emailTo)
+  {
+      if (emailTo.length > 0)
+      {
+        let emailList = emailTo.split(",")
+        for (var key in emailList) {
+          let email = emailList[key]
+          if(helper.validateEmail(email)){
+            emails.push(email)
+          }
+        }
+       res.json(emails)
+
+       // TODO:  send email by gmail smtp
+      }
+  }
+  // TODO:  return http status 
 }
