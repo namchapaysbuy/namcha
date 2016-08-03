ded6f967036dd1c61de7bce5ec53cda6953666e2
user5 <user5@gmail.com>
2016-08-03 10:30:28 +0700

    remove unused code

--- apps/controllers/page.js
+++ apps/controllers/page.js
@@ -1,18 +1,5 @@
 'use strict'
 
-require('rootpath')()
-
-let mongo = require('config/mongo')
-let User  = require('apps/models/user')
-
-exports.getMainpage = (req, res) => {
-  res.render('index.ejs', { title: 'oo' })
-} 
-
 exports.notfound = (req, res) => {
   res.send('404 notfound')
-}
-
-exports.getMe = (req, res) => {
-  res.send('Get me method...')
 }
