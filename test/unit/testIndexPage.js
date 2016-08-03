9c4b063a2e2f6e7a9e79a9e9783384e9a7eb8991
Archer <archer@Ashers-Mac-mini.local>
2016-08-03 17:31:47 +0700

    add testIndexPage.js

---
+++ test/unit/testIndexPage.js
@@ -0,0 +1,13 @@
+let index  = require('../../public/javascript/index.js');
+let chai = require('chai')
+let should = chai.should()
+
+describe('index.js', function() {
+  it('should return true when input email format', function(done) {
+    return new Promise(function (resolve) {
+        index.isAllEmail('pantakan@gmail.com').should.equal(true);
+        resolve();
+    })
+    .then(done());
+  })
+})
