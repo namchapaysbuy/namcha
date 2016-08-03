86729caabe8fdb59265ebf544c152f073c3b1b27
Khan <khan@Khans-Mac-mini.local>
2016-08-03 15:55:55 +0700

    update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,8 +1,9 @@
-cd /var/jenkins_home/workspace/namcha.dev.unit-test
-
 echo "get code"
 git pull origin master
 
 echo "build docker"
+docker build -t namcha:latest .
 
-echo "run docker"
+echo "re-run docker"
+docker rm -f namcha
+docker run --restart=always -p 80:3000 -d --name namcha namcha
