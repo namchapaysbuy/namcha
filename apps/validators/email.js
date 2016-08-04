270de4cce3d15b0e0486105c65b58c629e25ab06
user5 <user5@gmail.com>
2016-08-04 15:13:16 +0700

    add new validate for checking email body

--- apps/validators/email.js
+++ apps/validators/email.js
@@ -3,6 +3,9 @@
 module.exports = {
   validTopic: (topic) => {
     return topic.length <= 500
+  },
+  validBody: (body) => {
+    return body.length <= 5000
   }
   // format: (email) => {
   //   let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
