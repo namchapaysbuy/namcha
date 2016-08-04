1aa5d2463ed0198919454b0319a49baa4e602efa
Khan <khan@Khans-Mac-mini.local>
2016-08-04 11:06:35 +0700

    add recipent controller

--- apps/controllers/recipients.js
+++ apps/controllers/recipients.js
@@ -7,7 +7,21 @@ let helper = require('apps/helpers')
 let mongo = require('config/mongo')
 let User  = require('apps/models/user')
 
+
 exports.getMainpage = (req, res) => {
   res.render('recipients.ejs', { title: 'Recipients' })
+}
+
+exports.addRecipient = (req, res) => {
+  const result = {
+    code: 201,
+    message: 'Success',
+    recipient: {
+      firstName: 'David',
+      lastName: 'Beckham',
+      email: 'david.beckham@gmail.com'
+    }
+  }
+  res.send(result)
 }
 
