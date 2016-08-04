d25a296d51d5c8e9ce9228d2d0d164fe393e5f3d
Archer <archer@Ashers-Mac-mini.local>
2016-08-04 12:11:25 +0700

    add 2 test case

--- test/unit/testIndexPage.js
+++ test/unit/testIndexPage.js
@@ -3,23 +3,55 @@
 let index  = require('../../public/javascript/index.js');
 let chai = require('chai')
 let should = chai.should()
+let expect = chai.expect
+
+// var chaiAsPromised = require('chai-as-promised');
+// chai.use(chaiAsPromised);
 
 describe('index.js', function() {
 
-  it('should return true when input email format', function(done) {
-    return new Promise(function (resolve) {
-        index.isAllEmail('pantakan@gmail.com').should.equal(true);
-        resolve();
-    })
-    .then(done());
+  it('should return true when object in array is all email', function() {
+    return index.isAllEmail(['pantakan@gmail.com','somboon@gmail.com'])
+    .then(data => {
+        expect(data).to.equal(true)
+    })    
   })
 
-  it('should return false when input wrong email format ', function(done) {
-    return new Promise(function (resolve) {
-        index.isAllEmail('pantakangmail.com').should.equal(false);
-        resolve();
-    })
-    .then(done());
+  it('should return true when some object in array is not email', function() {
+    return index.isAllEmail(['pantakan@gmail.com','foo'])
+      .catch(error => {
+        expect(error).to.equal(false)
+      })
   })
   
+  it('should return true when email list more than 50', function() {
+
+    let emailList = ''
+    for(let i = 0; i <= 51; i++){ 
+        emailList += 'pantakan' + i + '@gmail.com,';
+    }
+    emailList += emailList.substring(emailList.length, emailList.length - 1);
+    emailList = emailList.split(',');
+ 
+    return index.isOverLimitedEmail(emailList)
+    .then(data => {   
+      expect(data).to.equal(true)
+    })
+  })
+
+  it('should return false when email list less than 50', function() {
+
+    let emailList = ''
+    for(let i = 0; i <= 1; i++){ 
+        emailList += 'pantakan' + i + '@gmail.com,';
+    }
+    emailList += emailList.substring(emailList.length, emailList.length - 1);
+    emailList = emailList.split(',');
+ 
+    return index.isOverLimitedEmail(emailList)
+    .catch(error => {   
+      expect(error).to.equal(false)
+    })
+  })
+
 })
