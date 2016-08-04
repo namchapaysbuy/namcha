5da633d4d2a36fb5d7492d090add3738f02c3b5d
Khan <khan@Khans-Mac-mini.local>
2016-08-04 17:35:10 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 echo "get code"
 git pull origin master
 
+echo "bower install"
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install -g bower
+bower install
+
 echo "build docker"
 docker build -t namcha:latest .
 
